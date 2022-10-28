349ecafaf43e7ec57a63a31b5eccdaa47022703b
dev4ult <nibrasbiasaaja@gmail.com>
2022-10-28 10:38:58 +0700

    adding form state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
 export default function Form() {
+  const [formData, setFormData] = React.useState({
+    firstText: '',
+    lastText: '',
+  });
+
+  function handleChange(event) {
+    const selectedInput = event.target.name;
+    const valueInput = event.target.value;
+    setFormData((prevFormData) => {
+      return { ...prevFormData, [selectedInput]: valueInput };
+    });
+  }
+
+  const { firstText, lastText } = formData;
+
   return (
-    <div className="form container">
+    <form className="form container">
       <div className="input-text flex">
-        <input type="text" placeholder="top text" />
-        <input type="text" placeholder="bottom text" />
+        <input type="text" placeholder="top text" onChange={handleChange} name="firstText" />
+        <input type="text" placeholder="bottom text" onChange={handleChange} name="lastText" />
       </div>
-    </div>
+      <p>
+        <span>{firstText}</span> <span>{lastText}</span>
+      </p>
+    </form>
   );
 }
